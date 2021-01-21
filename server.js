1f40f594ef3cca359e69cda96dcf5620151bb114
MartynasCepas <martynas.cepas@ktu.edu>
2021-01-21 17:42:28 +0200

    Changed Port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 80;
 
 var routes = require("./api/routes/routes"); //importing route
 routes(app); //register the route
